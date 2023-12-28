29addd205aee47ae248a3fdbf4815943cb709e2e
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-27 23:54:15 -0500

    App Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-        <Logo />
-    </div>
-  );
-}
+    return (
+      <Router>
+          <NavBar/>
+          <Routes>
 
+              
+          </Routes>
+      </Router>
+  
+    )
+  }
 export default App;
