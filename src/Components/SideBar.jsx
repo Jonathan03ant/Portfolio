95d77a837117d6fbd1ba417371432d99f160888d
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-27 23:56:22 -0500

    Side Bar

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const SideBar = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+
+
+export default SideBar;
