b54c43235e39381e8aa88ff0a89ff8b5b2365cc4
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-28 02:04:12 -0500

    App modification, Home page and Side bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import React, { useEffect } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import SideBar from './Components/SideBar';
+
+import Home from './Components/Routes/Home';
+import Projects from './Components/Routes/Projects';
+import Testimonies from './Components/Routes/Testimonies';
+import About from './Components/Routes/About';
 
 function App() {
     return (
-      <Router>
-          <NavBar/>
-          <Routes>
-
-              
-          </Routes>
-      </Router>
+        <Router>
+            <NavBar/>
+            <div className="flex">
+                <SideBar/>
+                
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/testimonies' element={<Testimonies />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </div>
+        </Router>
   
     )
-  }
+}
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
       ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            'rock-salt': ['Rock Salt', 'cursive'],
+        }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
