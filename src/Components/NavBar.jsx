bc125f86c93fe8f9a84274b511cfae3f9603318a
jonathan <jonathan03ant@gmail.com>
2024-09-04 12:40:15 -0400

    Navbar edit and Home

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -10,8 +10,8 @@ import logo from '../../public/img/logo.png';
 export default function NavBar() {
     return (
         <div className='nav flex justify-between w-full mt-10' >
-            <div className="navbar px-7 text-lg text-white">
-                <img src={logo} alt="logo" className='w-23 h-14 ml-10 mt-10'/>
+            <div className="navbar px-1 text-lg text-white">
+                <img src={logo} alt="logo" className='w-23 h-14 ml-10'/>
             </div>
 
             <div className="navbar px-2 mx-7 text-lg text-white space-x-2">
--- src/Components/Routes/Home.jsx
+++ src/Components/Routes/Home.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import {Typewriter} from 'react-simple-typewriter';
+import { FaAnglesLeft } from "react-icons/fa6";
 
 
 export default function Home() {
 
     const words = [
-        'A student at Carleton\n',
-        'Looking for my first COOP\n',
-        'I Love coding in C and C++\n',
-        'And I am learning React\n',
-        'I am a team player\n',
-        'continuous learner\n',
-        'a hard worker\n',
-        'and a Problem Solver\n',
+        "Jonathan's Resume\n",
+        'Check Out My Projects Section\n',
     ];
 
     return (
         <div className="projects flex flex-col items-center justify-start mt-10 mb-20 space-y-5 max-w-full">
             <div className=' mr-7 ms-2'>
-                <h1 className='font-zillo text-white text-3xl'>
-                    Hello My name is Jonathan 
-                </h1>
-                
-                <h1 className='font-zillo text-white text-2xl'>
+            <h1 className='font-zillo text-white text-2xl mb-10'>
                     <br/>
                     <span> 
                     <Typewriter
@@ -35,8 +26,13 @@ export default function Home() {
                         delaySpeed={1000}
                     />
                     </span>
-                   
                 </h1>
+                <h1 className='font-zillo text-white text-3xl'>
+                    Hello My name is Jonathan 
+                </h1>
+                <div>
+
+                </div>
             </div>
         </div>
     )
